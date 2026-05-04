fd74c354746957161991d46a81b9064ae2b51204
Abdullah-ML-ENG <2023-bs-cs-257@tuf.edu.pk>
2026-05-04 15:25:36 +0500

    Add config.py - Configuration for Windows J.A.R.V.I.S

---
+++ config.py
@@ -0,0 +1,16 @@
+"""
+J.A.R.V.I.S - Windows Voice Assistant
+Configuration file for customizing the assistant
+"""
+
+# User settings
+USERNAME = "Abdullah"  # Change to your name
+VOICE_SPEED = 150     # Words per minute (80-300)
+VOICE_VOLUME = 1.0    # Volume level (0.0-1.0)
+
+# Browser settings
+DEFAULT_BROWSER = "chrome"  # chrome, firefox, or edge
+
+# Application settings
+MAX_TASKS = 50  # Maximum number of tasks to store
+REMINDER_CHECK_INTERVAL = 5  # Check reminders every 5 seconds
